9002f06091b15b12f87a50a34b118fbbb37b9a0f
Azamat Mukhamejanov <mukhamejanov.aza@gmail.com>
2016-09-14 20:29:55 +0600

    Name calculator done

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 (function () {
   'use strict';
 
-  angular.module('myFirstApp', [])
+  angular.module('NameCalculator', [])
 
-  .controller('myFirstController', function($scope) {
-    $scope.name = "Azamat";
-    $scope.sayHello = function() {
-      return "Hello Coursera!";
+  .controller('nameCalculatorController', function($scope) {
+    $scope.name = "";
+    $scope.totalValue = 0;
+    $scope.displayNumeric = function() {
+      var totalNameValue = calcNumericForString($scope.name);
+      $scope.totalValue = totalNameValue;
+    }
+
+    function calcNumericForString(string) {
+      var totalStringValue = 0;
+      for (var i = 0; i < string.length; ++i)
+        totalStringValue += string.charCodeAt(i);
+
+      return totalStringValue;
     }
   });
+
 
 })();
